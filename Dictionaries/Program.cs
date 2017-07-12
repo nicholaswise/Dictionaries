7bfd9a9b9352cb3a38b69a877b034b66797364ed
Nick Wise <nickcwise@gmail.com>
2017-07-12 14:17:47 -0400

    updated exercises

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -10,6 +10,39 @@ namespace Dictionaries
     {
         static void Main(string[] args)
         {
+            //Syntax for dictionaries
+
+            Dictionary<string, string> snackMachine = new Dictionary<string, string>()
+            {
+                {"A1", "Twix"},
+                {"A2","Twizzlers" },
+                {"A3","Reeses" },
+                {"A4","Sour Patch Kids" }
+
+            };
+            foreach (KeyValuePair<string, string> item in snackMachine)
+            //foreach (var item in snackMachine)
+            {
+               // Console.WriteLine(item);
+            }
+
+            Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+
+            drinkMachine.Add(10, "Ginger Ale");
+            drinkMachine.Add(11, "Bottled Water");
+            drinkMachine.Add(12, "Orange Juice");
+            drinkMachine.Add(13, "Coke");
+            drinkMachine.Add(14, "Dr. Pepper");
+
+            //Counting the number of entries in the dictionary
+            //Console.WriteLine(drinkMachine.Count);
+
+            foreach (KeyValuePair<int,string> drink in drinkMachine)
+            {
+                Console.WriteLine(drink.Key);
+                Console.WriteLine(drink.Value);
+            }
+
         }
     }
 }
